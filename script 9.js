fa74ac44ee448a72caf39e65a5bbb0a75617f036
Basti <basti.babaeva@yandex.ru>
2024-11-08 23:48:05 +0700

    исправление ошибки в названии строки ввода

--- script 9.js
+++ script 9.js
@@ -2,18 +2,21 @@ let equal_button=false;
 
 // Обработчики событий для кнопок ввода цифр
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelectorAll("button[class*='btn_']").forEach(element=>{
-        element.addEventListener('click', function(element){
-            if(equal_button){
-                document.querySelector('input').value="";
-                document.querySelector('.grid-input').textContent="";
-                equal_button=false;
-            }
-            if(isNaN(parseFloat(document.querySelector('input').value.slice(-1)))===true&&document.querySelector('input').value.slice(-1)!=='.')
+    // Обработчики событий для кнопок ввода цифр
+    document.querySelectorAll("button[class *='btn_']").forEach(element =>{
+        element.addEventListener('click',function(element){
+            if(equal_button)
             {
-                document.querySelector('.gray-input').textContent="";
+                document.querySelector('input').value = "";
+                document.querySelector('.gray-input').textContent = "";
+                equal_button = false;
+            }
+            if(isNaN(parseFloat(document.querySelector('input').value.slice(-1))) === true && document.querySelector('input').value.slice(-1) !== '.')
+            {
+                document.querySelector('.gray-input').textContent = "";
             }
             document.querySelector('input').value+=element.target.textContent;
+            document.querySelector('.gray-input').textContent+=element.target.textContent;
         })
     });
 
